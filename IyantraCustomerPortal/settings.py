cc2ffd919f683710339a0b4bacf682ca2cd63dec
akhil kishor <b.akhil072@gmail.com>
2022-02-28 11:41:09 +0530

    new changesssss

--- IyantraCustomerPortal/settings.py
+++ IyantraCustomerPortal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l$_ycpchudtbf5yc6j#oimnbzi+nmw28a-bd047j9wts%-dr+f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
