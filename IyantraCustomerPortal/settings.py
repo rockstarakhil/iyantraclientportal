7ec69b426477fae0b52267782d31b3cc445027c4
akhil kishor <b.akhil072@gmail.com>
2022-02-28 17:21:12 +0530

    static files are added

--- IyantraCustomerPortal/settings.py
+++ IyantraCustomerPortal/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 ]
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
