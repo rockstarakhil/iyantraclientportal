d227fdd4841b9795c76d57d1534d66e9be2c0a29
akhil kishor <b.akhil072@gmail.com>
2022-02-28 16:05:17 +0530

    static files are added

--- IyantraCustomerPortal/settings.py
+++ IyantraCustomerPortal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l$_ycpchudtbf5yc6j#oimnbzi+nmw28a-bd047j9wts%-dr+f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
