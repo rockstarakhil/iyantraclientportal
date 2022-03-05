88f513e577aa60f9f5782e2ee1f50e971ce91a82
akhil kishor <b.akhil072@gmail.com>
2022-03-05 12:48:58 +0530

    500 error resolved

--- IyantraCustomerPortal/settings.py
+++ IyantraCustomerPortal/settings.py
@@ -57,9 +57,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'IyantraCustomerPortal.urls'
 os.path.join(os.path.dirname(__file__) ,'../templates').replace('\\','/')
-TEMPLATE_DIRS = (
-    'os.path.join(BASE_DIR, "templates"),'
-)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
